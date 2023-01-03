587f6f5a0e326488feebc12b69fa84cf2ef94bdd
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2023-01-03 18:39:49 +0000

    about me text changes

--- portfolio/src/components/AboutMe/AboutMe.jsx
+++ portfolio/src/components/AboutMe/AboutMe.jsx
@@ -6,14 +6,14 @@ export default function AboutMe() {
         <div className="AboutMe" id="about-me">
         <div className="bio">
             <div className="bio-title"> 
-            <h2>i love problem solving, </h2>
-            <h2>creating magic with code  </h2>
-            <h2>& cocker spaniels.</h2>
+            <h2>i love problem solving with Python, </h2>
+            <h2>creating magic with frontend CSS and Javascript  </h2>
+            <h2>& i adore cocker spaniels.</h2>
             </div>
 
             <div className="bio-body">
             <div className="bio-text">
-            <span>  I am a junior software developer, UX experience designer.</span> 
+            <span>  I am a graduate software developer, UX experience designer.</span> 
             I am a natural problem solver and am able to use my UX and coding knowledge to create a gret user experience.</div>
             <div className="bio-text">
             <span>I love building and designing,</span> I have a keen eye for fine detail & simple design.
