b22cdf598b5fa6e219de767b2fa9a60e8b79e9fc
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2023-01-04 19:06:41 +0000

    reformatting font and padding

--- portfolio/src/components/AboutMe/AboutMe.jsx
+++ portfolio/src/components/AboutMe/AboutMe.jsx
@@ -6,19 +6,19 @@ export default function AboutMe() {
         <div className="AboutMe" id="about-me">
         <div className="bio">
             <div className="bio-title"> 
-            <h2>i love problem solving with Python,</h2>
+            <h2>i love problem solving with <span>Python</span>,</h2>
                  
-            <h2>creating magic with frontend CSS and Javascript  </h2>
+            <h2>creating magic with frontend <span>CSS</span> and <span>Javascript</span>  </h2>
             <h2>
-                and i adore cocker spaniels.</h2>
+                and i absolutely adore cocker spaniels.</h2>
             </div>
 
             <div className="bio-body">
             <div className="bio-text">
-            <span>  I am a graduate software developer and UX experience designer.</span> 
+            <span>  I am a graduate software developer </span>and UX experience designer. 
             I am a natural problem solver and am able to use my UX and coding knowledge to create a great user experience.</div>
             <div className="bio-text">
-            <span>I love building and designing and</span> I have a keen eye for fine detail & simple design.
+            <span>I love building and designing </span> and I have a keen eye for fine detail & simple design.
             </div>
             
             </div>
