b045e23d499616ce321475b97f79c415807f5ed8
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2023-01-04 16:40:46 +0000

    image srcs

--- portfolio/src/components/Menu/Menu.jsx
+++ portfolio/src/components/Menu/Menu.jsx
@@ -22,7 +22,7 @@ export default function Menu( {menuOpen, setMenuOpen}) {
             </li>
             <li>
             <a href="https://www.linkedin.com/in/vaishali7/" className="linkedin">
-            <img src="/VM-portfolio/assets/linkedin.svg" alt="clickable linkedin icon"/></a>
+            <img src="/assets/linkedin.svg" alt="clickable linkedin icon"/></a>
            
             </li>
         </ul>
