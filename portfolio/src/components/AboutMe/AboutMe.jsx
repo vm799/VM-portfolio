2123f28b4b4f2b607cfaecd8b89adaf79a38ff61
Vaishali <vaishalimehmi@yahoo.co.uk>
2023-01-21 16:33:06 +0000

    altering margin top

--- portfolio/src/components/AboutMe/AboutMe.jsx
+++ portfolio/src/components/AboutMe/AboutMe.jsx
@@ -16,7 +16,7 @@ export default function AboutMe() {
             <div className="bio-body">
             <div className="bio-text">
             <span>  I am a graduate software developer </span>and UX experience designer. 
-            I am a natural problem solver and am able to use my UX and coding knowledge to create a great user experience.</div>
+            I am a natural problem solver and am able to use my UX and coding knowledge to create great user experiences.</div>
             <div className="bio-text">
             <span>I love building and designing </span> and I have a keen eye for fine detail & simple design.
             </div>
