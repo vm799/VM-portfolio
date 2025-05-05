be9563a5bd692c9ef46d7d74178758c6b70dcdbf
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2025-05-05 15:11:32 +0100

    Update AboutMe.jsx
    Added new AI capabilites

--- portfolio/src/components/AboutMe/AboutMe.jsx
+++ portfolio/src/components/AboutMe/AboutMe.jsx
@@ -6,17 +6,17 @@ export default function AboutMe() {
         <div className="AboutMe" id="about-me">
         <div className="bio">
             <div className="bio-title"> 
-            <h2>i love testing software and problem solving with <span>Java and Python</span>,</h2>
+            <h2>i love building software, finding solutions, managing projects and problem solving with <span>AI.</span>,</h2>
                  
-            <h2>creating magic with frontend <span>CSS</span> and <span>Javascript</span>  </h2>
+            <h2>creating magic with  <span>CSS</span> all things <span>AI</span>  </h2>
             <h2>
                 and i absolutely adore cocker spaniels.</h2>
             </div>
 
             <div className="bio-body">
             <div className="bio-text">
-            <span>  I am a graduate software developer </span>and UX experience designer. 
-            I am a natural problem solver and am able to use my UX and coding knowledge to create great user experiences.</div>
+            <span>  I am a Delivery Lead and hobbist AI software developer </span>and UX experience designer. 
+            I am a natural problem solver and am able to use my AI implemenatation strategies, UX and coding knowledge to create great user experiences.</div>
             <div className="bio-text">
             <span>I love designing, building and testing </span> and I have a keen eye for fine detail & simple design.
             </div>
