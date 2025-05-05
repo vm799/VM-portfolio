68b057b5ae20ef796bd5aff25a68fb0957967ef0
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2025-05-05 15:38:13 +0100

    Update Menu.jsx

--- portfolio/src/components/Menu/Menu.jsx
+++ portfolio/src/components/Menu/Menu.jsx
@@ -6,7 +6,7 @@ export default function Menu( {menuOpen, setMenuOpen}) {
     <div className={ "menu  " + (menuOpen && "active" )}>
         <ul onClick={()=>setMenuOpen(false)}  >
             <li>
-                <a href="https://www.canva.com/design/DAGmfSYJhDk/S-aoce0kY37t0WMyn2gs6Q/view?utm_content=DAGmfSYJhDk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h12da85989f" </a>my CV</a>
+                <a href="https://www.canva.com/design/DAGmfSYJhDk/S-aoce0kY37t0WMyn2gs6Q/view?utm_content=DAGmfSYJhDk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h12da85989f"> my CV</a>
             </li>
             <li>
                 <a href="#portfolio"> my portfolio</a>
